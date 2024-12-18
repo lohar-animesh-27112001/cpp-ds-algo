4a4b5bc2204eb96e191b6baf7197d66558aea55a
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-12-18 11:12:31 +0530

    Update basic_class.cpp

--- Basics/basic_class.cpp
+++ Basics/basic_class.cpp
@@ -5,25 +5,41 @@ class Person {
 protected:
     string name;
     string gender;
+    int age;
 public:
-    void set_name(string name) {
+    Person(int age) : age(age) {}
+    void set_name(const string& name) {
         this->name = name;
     }
-    void set_gender(string gender) {
+    void set_gender(const string& gender) {
         this->gender = gender;
+    }
+    void display() const {
+        cout << "Name: " << name << ", Gender: " << gender << ", Age: " << age << endl;
     }
 };
 
-class Male: public Person {
+class Male : public Person {
 protected:
     string profession;
 public:
-    void set_profession(string profession) {
-    this->profession = profession;
+    Male(const string& name, const string& gender, int age) : Person(age) {
+        this->set_name(name);
+        this->set_gender(gender);
+    }
+    void set_profession(const string& profession) {
+        this->profession = profession;
+    }
+    void display() const {
+        Person::display();
+        cout << "Profession: " << profession << endl;
     }
 };
 
 int main() {
-    Male person1;
-    person1.set_profession("S/W engineer");
+    Male person1("Animesh", "Male", 20);
+    person1.set_profession("S/W Engineer");
+    person1.set_name("Animesh Lohar");
+    person1.display();
+    return 0;
 }
